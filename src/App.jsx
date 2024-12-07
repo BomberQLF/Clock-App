7a68cd54bb1dffd0f25f5c3e51db8eba27663984
BomberQLF <tmurphyparis@gmail.com>
2024-12-07 16:49:08 +0100

    Quote API intégré

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Quote from './Components/Quote';
 
 function App() {
-
+  return (
+    <div className="App">
+      < Quote />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Components/Quote.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import RefreshIcon from '../../src/assets/desktop/icon-refresh.svg';
+
+const Quote = () => {
+    const [quote, setQuote] = useState(null);
+
+    // Fonction pour appeler l'API
+    const quoteFinder = async () => {
+        try {
+            const response = await fetch('https://api.quotable.io/random');
+            if (!response.ok) {
+                throw new Error(`HTTP Error ! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            setQuote(data);
+        } catch (error) {
+            console.error('Erreur lors du fetch : ', error);
+        }
+    };
+
+    // Appel initial lors du montage du composant
+    useEffect(() => {
+        quoteFinder();
+    }, []);
+
+    console.log(quote ? quote : "En attente de la citation...");
+
+    return (
+        <div className="quote-container" style={{ width: '30rem', padding: '4rem' }}>
+            {quote ? (
+                <div>
+                    <p className="quote-content">"{quote.content}"</p>
+                    <span style={{ fontWeight: 'bold' }} className="quote-author">{quote.author}</span>
+                    <img 
+                        onClick={quoteFinder} 
+                        src={RefreshIcon} 
+                        alt="Refresh quote" 
+                        style={{ cursor: "pointer" }}
+                    />
+                </div>
+            ) : (
+                <p>Quote loading ...</p>
+            )}
+        </div>
+    );
+};
+
+export default Quote;
